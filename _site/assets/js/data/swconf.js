9db12030701d16fe9526c18f1328bc830cb8f792
nayounho <nnyyhh9247@gmail.com>
2024-02-22 17:07:26 +0900

    basic set blog

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1707899499',resources: [
+    cacheName: 'chirpy-1708586549',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
@@ -16,8 +16,6 @@ const swconf = {
       
       
     ],allowHosts: [
-      
-        'chirpy-img.netlify.app',
       
           'fonts.googleapis.com',
           'fonts.gstatic.com',
