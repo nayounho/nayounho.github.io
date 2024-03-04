367cf628869bb4343194cb0a57d8442d9c895180
nayounho <nnyyhh9247@gmail.com>
2024-03-04 18:29:12 +0900

    20240304 code factory ch11

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709023657',resources: [
+    cacheName: 'chirpy-1709544475',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
